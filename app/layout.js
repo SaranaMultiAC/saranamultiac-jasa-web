a12bdb21384770d1dd9e71cd39c3f5e4ebd5ffa5
SaranaMultiAC <saranamultikejayaan30@gmail.com>
2026-06-23 17:36:21 +0700

    Add RootLayout component with metadata for app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Sarana Multi AC',
+  description: 'Jasa Service AC Bekasi',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
