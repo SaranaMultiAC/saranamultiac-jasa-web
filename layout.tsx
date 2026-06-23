bd3ad81acca277e85e4c06afd1a5611439ae42a3
SaranaMultiAC <saranamultikejayaan30@gmail.com>
2026-06-23 15:04:30 +0700

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: 'Jasa Service AC Jakarta Bergaransi - Sarana Multi AC',
-  description: 'Cuci AC 85rb, Isi Freon 150rb, Bongkar Pasang 350rb. Teknisi berpengalaman. Garansi 30 hari. Hub 08137321066'
-}
-export default function RootLayout({children}: {children: React.ReactNode}) {
-  return (<html lang="id"><body style={{margin:0}}>{children}</body></html>)
-}
