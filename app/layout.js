7c74650514a88be98494ea2c740414a506750377
SaranaMultiAC <saranamultikejayaan30@gmail.com>
2026-06-25 15:44:18 +0700

    Add global CSS import to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import './globals.css'
+
 export const metadata = {
   title: 'Service AC Cilangkap Jakarta Timur - Sarana Multi AC',
   description: 'Jasa service AC panggilan Cilangkap Jaktim. Cuci AC, isi freon, perbaikan AC. Teknisi berpengalaman, harga transparan, garansi 60 hari. Hubungi WA 0813-732-1066',
